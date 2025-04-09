10312c5ded9b0ff958ebe6b4aa8605260d5e7b20
Sina Roomi <sroomi2004@gmail.com>
2025-04-09 19:12:02 -0400

    Rewrote getting playlist function, created functions to get a playlist, get a song, find a song, and get a song playlist

--- src/playlist.py
+++ src/playlist.py
@@ -3,73 +3,61 @@ import json
 
 
 
-class Playlist:
+class Temp_name:
 
-    def __init__(self, api_client, playlist_id):
+    # TODO DO check if valid parameters were passed
+    def __init__(self, api_client, playlist_id, create_file=False):
         self.api_client = api_client
         self.playlist_id = playlist_id
 
-    def getSongs(self, size):
-        #see information in json file
-        # with open("spotifyInfo.json", "w") as outfile: 
-        #     json.dump(user_playlist["items"], outfile, indent=1)
 
-        playlist_offset = 0
-        keepIterate = True
+        # The function 'playlist' will fetch a playlist with the given playlist id. Returns a dict
+        self.playlist = self.api_client.playlist(self.playlist_id)
 
-        list_of_songs = []
+        # Get number of songs in playlist
+        self.playlist_size = self.playlist['tracks']['total']
+        
+        
+        if create_file == True:
+            with open('playlist_data.json', 'w') as json_file:
+                json.dump(self.playlist, json_file)
+        
 
-        while (keepIterate):
-            user_playlist = (self.api_client).playlist_items(playlist_id=id, offset=playlist_offset)
-            for index, elem in enumerate(user_playlist["items"]):
-                # “uri”:”4iV5W9uYEdYUVa79Axb7Rh”, “positions”:[2] 
-                list_of_songs.append((elem["track"]["name"], {"uri":elem["track"]["uri"],"positions":[index+playlist_offset]}))
+    
+    def get_playlist(self):
+        return self.playlist
 
-            
-            if (playlist_offset + 100 > size):
-                keepIterate = False
-            else:
-                playlist_offset += 100
-            
-        return list_of_songs
+    def get_songs(self):
+        """
+        Returns a list of song names that were found in the given playlist
+        """
+        song_list = []
 
-    def removeDuplicate(self, songs,playlist_id):
-        # Data
-        playlist_id = playlist_id # The ID of the playlist containing the tracks to be deleted
+        for item in self.playlist['tracks']['items']:
+            song_list.append(item['track']['name'])
+        
+        return song_list
+        
+        
+    def find_song(self, song_name, song_list=None):
+        """
+        Takes in a song name and searches for that song. Uses playlist from given playlist_id.
+        If a song list was passed as a parameter, iterate over the song list
+        """
 
-        track_ids = []
-        seen = []
+        if not song_name:
+            print("Need valid song name.")
+            return None
+    
+        for item in self.playlist['tracks']['items']:
+            if item['track']['name'] == song_name:
+                return item['track']
+        
+        return None
+    
+    def add_song(self):
+        pass
 
-        for i in songs:
-            if (i[0] in seen):
-                track_ids.append(i[1])
-            else:
-                seen.append(i[0])
-
-        # Call the track deletion function
-        if (track_ids):
-            sp.playlist_remove_specific_occurrences_of_items(playlist_id=playlist_id, items=track_ids)
-
-
-    def addSongs(self, sp, playlist_id):
-        list_of_id = []
-
-        user_input = ""
-        stop = False
-        print("Enter URL of songs, type quit to stop")
-
-        while (stop == False):
-            
-            user_input = input(user_input)
-
-            if (user_input == "quit"):
-                stop = True
-                break
-            else:
-                list_of_id.append(user_input)
-
-            user_input = ""
-
-
-        if (list_of_id):
-            sp.playlist_add_items(playlist_id=playlist_id, items=list_of_id)
+    def remove_song(self):
+        pass
+    
